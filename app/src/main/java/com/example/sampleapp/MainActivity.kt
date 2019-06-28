6ea3350c5f0bb1433b1e94935547888573e5cc6c
Taslim <hridoy.bubd@gmail.com>
2019-06-28 14:36:52 +0600

    java to kotline

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.sampleapp
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    var tv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        tv = findViewById(R.id.homeScreentext)
+        setSupportActionBar(toolbar)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                    HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.home_fragment)
+        }
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                    HomeFragment()).commit()
+        } else if (id == R.id.nav_gallery) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                    GalleryFragment()).commit()
+
+        } else if (id == R.id.nav_slideshow) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+                    SlideFragment()).commit()
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+        } else if (id == R.id.nav_send) {
+            Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show()
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
